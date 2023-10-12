c6a756b53cbd3ab7a078965cff868199bb206521
harshmehta10 <68213602+harshmehta10@users.noreply.github.com>
2023-10-12 19:15:33 +0530

    weather app

--- script.js
+++ script.js
@@ -1,57 +1,17 @@
-const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
-const quoteDisplayElement = document.getElementById("quoteDisplay");
-const quoteInputElement = document.getElementById("quoteInput");
-const timerElement = document.getElementById("timer");
+const url =
+  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
 
-quoteInputElement.addEventListener("input", () => {
-  const arrayQuote = quoteDisplayElement.querySelectorAll("span");
-  const arrayValue = quoteInputElement.value.split("");
-
-  let correct = true;
-  arrayQuote.forEach((characterSpan, index) => {
-    const character = arrayValue[index];
-    if (character == null) {
-      characterSpan.classList.remove("correct");
-      characterSpan.classList.remove("incorrect");
-      correct = false;
-    } else if (character === characterSpan.innerText) {
-      characterSpan.classList.add("correct");
-      characterSpan.classList.remove("incorrect");
-    } else {
-      characterSpan.classList.remove("correct");
-      characterSpan.classList.add("incorrect");
-      correct = false;
-    }
-  });
-  if (correct) renderNewQuote();
-});
-
-function getRandomQuote() {
-  return fetch(RANDOM_QUOTE_API_URL)
-    .then((Response) => Response.json())
-    .then((data) => data.content);
+try {
+  const response = await fetch(url, options);
+  const result = await response.text();
+  console.log(result);
+} catch (error) {
+  console.error(error);
 }
-
-async function renderNewQuote() {
-  const quote = await getRandomQuote();
-  quoteDisplayElement.innerText = "";
-  quote.split("").forEach((character) => {
-    const characterSpan = document.createElement("span");
-    characterSpan.innerText = character;
-    quoteDisplayElement.appendChild(characterSpan);
-  });
-  quoteInputElement.value = null;
-  startTimer();
-}
-let startTime;
-function startTimer() {
-  timerElement.innerText = 0;
-  startTime = new Date();
-  setInterval(() => {
-    timer.innerText = getTimerTime();
-  }, 1000);
-}
-function getTimerTime() {
-  return Math.floor((new Date() - startTime) / 1000);
-}
-renderNewQuote();
