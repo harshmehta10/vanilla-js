682653cc38987d2046a33f72855d8d0bd7e3dc65
harshmehta10 <harshmehta.srm2002@gmail.com>
2023-10-15 14:32:05 +0530

    fixed

--- Weather app/script.js
+++ Weather app/script.js
@@ -1,17 +1,24 @@
 const url =
-  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle";
+  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Dehli";
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+    "X-RapidAPI-Key": "230a5a99acmsh09cb740609a8291p1a07e8jsn8c578d221392",
     "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
   },
 };
 
-try {
-  const response = await fetch(url, options);
-  const result = await response.text();
-  console.log(result);
-} catch (error) {
-  console.error(error);
-}
+// try {
+//   const response = await fetch(url, options);
+//   const result = await response.text();
+//   console.log(result);
+// } catch (error) {
+//   console.error(error);
+// }
+
+fetch(url, options)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("[data]", data);
+  })
+  .catch((e) => console.log("[err]", e));
