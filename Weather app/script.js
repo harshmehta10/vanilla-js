de72a5f63edb64e603effb2200fcb47cc2ec6ada
harshmehta10 <harshmehta.srm2002@gmail.com>
2023-12-04 12:04:53 +0530

    completed

--- Weather app/script.js
+++ Weather app/script.js
@@ -7,13 +7,6 @@ const options = {
   },
 };
 
-// try {
-//   const response = await fetch(url, options);
-//   const result = await response.text();
-//   console.log(result);
-// } catch (error) {
-//   console.error(error);
-// }
 const getWeather = (city) => {
   cityName.innerHTML = city;
   fetch(url + city, options)
